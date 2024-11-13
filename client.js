3241c83de66abca291a421b683be9ebb84203a52
beva3 <mizabienvenu@gmail.com>
2024-11-13 21:21:01 +0530

    display update

--- client.js
+++ client.js
@@ -1,3 +1,5 @@
+
+
 class Client{
     constructor(id,name,email,phone){
         this.id = id;
@@ -7,13 +9,17 @@ class Client{
     }
     
     // displays client information
-    display_client(){
-        console.log('-'.repeat(10));
-        console.log(`ID: ${this.id}`);
-        console.log(`Name: ${this.name}`);
-        console.log(`Email: ${this.email}`);
-        console.log(`Phone: ${this.phone}`);
-        console.log('-'.repeat(10));
+    display(){
+        const tableBody = document.getElementById('clientTableBody')
+        let rows = document.createElement("tr");
+        rows.innerHTML = `
+            <td class="highlight">${this.id}</td>
+            <td>${this.name}</td>
+            <td><a href="mailto:${this.email}">${this.email}</a></td>
+            <td>${this.phone}</td>
+        `;
+        // rows.classList.add('hightlight');
+        tableBody.appendChild(rows);
     }
 }
 
@@ -22,11 +28,16 @@ clients = [
     new Client(1, 'John Doe', 'john.doe@example.com', '123-456-7890'),
     new Client(2, 'Jane Smith', 'jane.smith@example.com', '987-654-3210'),
     new Client(3, 'Alice Johnson', 'alice.johnson@example.com', '555-555-5555'),
-    new Client(4, 'Bob Brown', 'bob.brown@example.com', '777-777-7777')
+    new Client(4, 'Bob Brown', 'bob.brown@example.com', '777-777-7777'),
+    new Client(5, 'Emily White', 'emily.white@example.com', '111-222-3333'), 
+    new Client(6, 'Michael Green', 'michael.green@example.com', '444-555-6666'), 
+    new Client(7, 'Jessica Blue', 'jessica.blue@example.com', '777-888-9999'), 
+    new Client(8, 'David Black', 'david.black@example.com', '000-111-2222')
 ]
 
 // Display all clients
-console.log('All Clients:');
+// console.log('All Clients:');
 for (let i = 0; i < clients.length; i++) {
-    clients[i].display_client()
+    console.log(clients[i].name);
+    clients[i].display()
 }
